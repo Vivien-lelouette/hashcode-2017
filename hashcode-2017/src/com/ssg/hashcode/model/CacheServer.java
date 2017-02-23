4d74a9e29eb6021af38581eeb9c8a7c1308f5a92
Sy Alioune <alioune.sy@soprasteria.com>
2017-02-23 20:12:36 +0100

    Adding cache capacity

--- hashcode-2017/src/com/ssg/hashcode/model/CacheServer.java
+++ hashcode-2017/src/com/ssg/hashcode/model/CacheServer.java
@@ -2,4 +2,11 @@ package com.ssg.hashcode.model;
 
 public class CacheServer {
 	private int id;
+	
+	private int capacity;
+
+	public CacheServer(int id, int capacity) {
+		this.id = id;
+		this.capacity = capacity;
+	}
 }
